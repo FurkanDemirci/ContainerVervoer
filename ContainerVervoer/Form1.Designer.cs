c017cc733a4417885a839f02825a19091b6cfd25
Furkan Demirci <furkan.demirci@live.nl>
2018-05-24 09:13:47 +0200

    Form design in progress

--- ContainerVervoer/Form1.Designer.cs
+++ ContainerVervoer/Form1.Designer.cs
@@ -28,13 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbPlace1 = new System.Windows.Forms.ListBox();
+            this.lbPlace4 = new System.Windows.Forms.ListBox();
+            this.listBox9 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbPlace2 = new System.Windows.Forms.ListBox();
+            this.lbPlace5 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lbPlace3 = new System.Windows.Forms.ListBox();
+            this.lbPlace6 = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbPlace1
+            // 
+            this.lbPlace1.FormattingEnabled = true;
+            this.lbPlace1.Location = new System.Drawing.Point(6, 19);
+            this.lbPlace1.Name = "lbPlace1";
+            this.lbPlace1.Size = new System.Drawing.Size(180, 69);
+            this.lbPlace1.TabIndex = 0;
+            // 
+            // lbPlace4
+            // 
+            this.lbPlace4.FormattingEnabled = true;
+            this.lbPlace4.Location = new System.Drawing.Point(6, 94);
+            this.lbPlace4.Name = "lbPlace4";
+            this.lbPlace4.Size = new System.Drawing.Size(180, 69);
+            this.lbPlace4.TabIndex = 3;
+            // 
+            // listBox9
+            // 
+            this.listBox9.FormattingEnabled = true;
+            this.listBox9.Location = new System.Drawing.Point(813, 118);
+            this.listBox9.Name = "listBox9";
+            this.listBox9.Size = new System.Drawing.Size(168, 238);
+            this.listBox9.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbPlace1);
+            this.groupBox1.Controls.Add(this.lbPlace4);
+            this.groupBox1.Location = new System.Drawing.Point(6, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(199, 179);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Front ship";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbPlace2);
+            this.groupBox2.Controls.Add(this.lbPlace5);
+            this.groupBox2.Location = new System.Drawing.Point(211, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(199, 179);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Middle ship";
+            // 
+            // lbPlace2
+            // 
+            this.lbPlace2.FormattingEnabled = true;
+            this.lbPlace2.Location = new System.Drawing.Point(6, 19);
+            this.lbPlace2.Name = "lbPlace2";
+            this.lbPlace2.Size = new System.Drawing.Size(180, 69);
+            this.lbPlace2.TabIndex = 0;
+            // 
+            // lbPlace5
+            // 
+            this.lbPlace5.FormattingEnabled = true;
+            this.lbPlace5.Location = new System.Drawing.Point(6, 94);
+            this.lbPlace5.Name = "lbPlace5";
+            this.lbPlace5.Size = new System.Drawing.Size(180, 69);
+            this.lbPlace5.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lbPlace3);
+            this.groupBox3.Controls.Add(this.lbPlace6);
+            this.groupBox3.Location = new System.Drawing.Point(416, 19);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(199, 179);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Back ship";
+            // 
+            // lbPlace3
+            // 
+            this.lbPlace3.FormattingEnabled = true;
+            this.lbPlace3.Location = new System.Drawing.Point(6, 19);
+            this.lbPlace3.Name = "lbPlace3";
+            this.lbPlace3.Size = new System.Drawing.Size(180, 69);
+            this.lbPlace3.TabIndex = 0;
+            // 
+            // lbPlace6
+            // 
+            this.lbPlace6.FormattingEnabled = true;
+            this.lbPlace6.Location = new System.Drawing.Point(6, 94);
+            this.lbPlace6.Name = "lbPlace6";
+            this.lbPlace6.Size = new System.Drawing.Size(180, 69);
+            this.lbPlace6.TabIndex = 3;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.groupBox1);
+            this.groupBox4.Controls.Add(this.groupBox3);
+            this.groupBox4.Controls.Add(this.groupBox2);
+            this.groupBox4.Location = new System.Drawing.Point(146, 361);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(622, 203);
+            this.groupBox4.TabIndex = 10;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Ship";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Location = new System.Drawing.Point(114, 85);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 100);
+            this.groupBox5.TabIndex = 11;
+            this.groupBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1051, 611);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.listBox9);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbPlace1;
+        private System.Windows.Forms.ListBox lbPlace4;
+        private System.Windows.Forms.ListBox listBox9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lbPlace2;
+        private System.Windows.Forms.ListBox lbPlace5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox lbPlace3;
+        private System.Windows.Forms.ListBox lbPlace6;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
     }
 }
 
